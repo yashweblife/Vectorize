44369c502b34947436ddeaf9c205de1197b8d1fc
yashweblife <yashweblife20@gmail.com>
2022-06-26 14:07:05 -0500

    added some vector code and initialized webpack

---
+++ src/Vectorize.ts
@@ -0,0 +1,13 @@
+class Vector{
+    public x:number;
+    public y:number;
+    public z:number;
+    public mag:number;
+    public angle:number;
+
+    constructor(x:number=0,y:number=0,z:number=0){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
